11c7559463236238d68c1a7ab29714494276fbef
Anand J <anand.j@flipkart.com>
2016-06-20 18:35:50 +0530

    Mysql and kafka config changes

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,6 +6,19 @@ ROOT_DIR="/root"
 SETUP_CONF=$SYNC_DIR/vagrant/setup.conf
 SETUP_SCRIPT=$SYNC_DIR/vagrant/setup.sh
 
+# lib functions
+function add_to_file_if_not_already {
+	FILE=$1
+	LINE=$2
+	(grep "$LINE" $FILE) || (echo -e "$LINE" >> $FILE)
+}
+
+function edit_file_if_not_already {
+	FILE=$1
+	FROM_LINE=$2
+	TO_LINE=$3
+	(egrep "$FROM_LINE" $FILE) || (sed -i "s/$FROM_LINE/$TO_LINE/g" $FILE)
+}
 
 pushd $ROOT_DIR
 
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,5 +1,10 @@
 # MySql setup
-service mysql start
+edit_file_if_not_already $MYSQL_CONF "bind-address\s*= 127.0.0.1" "bind-address = 0.0.0.0"
+if [ ! `grep "skip-grant-tables" $MYSQL_CONF` ]; then
+	echo "[mysqld]" >> $MYSQL_CONF
+	echo "skip-grant-tables" >> $MYSQL_CONF
+fi
+service mysql restart
 
 # Kafka setup
 if [ ! -d $KAFKA_DIR ]; then
@@ -8,10 +13,11 @@ if [ ! -d $KAFKA_DIR ]; then
 fi
 
 pushd $KAFKA_DIR
+add_to_file_if_not_already $KAFKA_CONFIG "advertised.host.name=127.0.0.1"
 (./bin/kafka-server-stop.sh) || true
 (./bin/zookeeper-server-stop.sh) || true
-./bin/zookeeper-server-start.sh config/zookeeper.properties &
-./bin/kafka-server-start.sh config/server.properties &
+./bin/zookeeper-server-start.sh $ZK_CONFIG &
+./bin/kafka-server-start.sh $KAFKA_CONFIG &
 sleep 4
 for t in $KAFKA_TOPICS; do
 	./bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic $t
